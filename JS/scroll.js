976feb3548ccbd8fcc026bd5e8691ca800b14969
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-10-08 10:12:43 +0300

    Add

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,6 +1,12 @@
 const projectsContainer = document.querySelector(".projects-container");
 const projectsBtn = document.querySelector('.projects-btn')
+const contactBtn = document.querySelector('.contact-btn')
+
 
 projectsBtn.addEventListener('click', e => {
     projectsContainer.scrollIntoView({behavior: "smooth"});
+})
+
+contactBtn.addEventListener('click', e => {
+
 })
